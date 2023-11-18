8b9ce0125dfaa3cd04ea59b28dbd0313e6fa22a5
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-11-17 21:48:14 -0600

    added temporary holiday bus endpoint

---
+++ endpoints/holidaybus/index.js
@@ -0,0 +1,123 @@
+const fetch = require('node-fetch');
+
+require('dotenv').config();
+
+let busNum = 8273;
+
+const isDST = (date) => {
+  const dstStart = new Date(date.getFullYear(), 2, 8, 2, 0, 0); // Spring forward
+  const dstEnd = new Date(date.getFullYear(), 10, 1, 1, 0, 0); // Fall back
+
+  if (date >= dstStart && date < dstEnd) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const getOffset = (date) => {
+  if (isDST(date)) return '-05:00';
+
+  return '-06:00';
+}
+
+const parseTime = (raw) => {
+  const year = raw.substring(0, 4);
+  const month = raw.substring(4, 6);
+  const day = raw.substring(6, 8);
+
+  const time = raw.substring(9, 17);
+  const offset = getOffset(new Date());
+
+  return new Date(`${year}-${month}-${day}T${time}${offset}`).valueOf();
+};
+
+
+const update = async () => {
+  try {
+    const now = new Date();
+
+    const vehicleReq = await fetch(`https://ctabustracker.com/bustime/api/v3/getvehicles?key=${process.env.cta_bus_auth}&format=json&vid=${busNum}`);
+    const predictionsReq = await fetch(`https://ctabustracker.com/bustime/api/v3/getpredictions?key=${process.env.cta_bus_auth}&format=json&vid=${busNum}&tmres=s`);
+    const routesReq = await fetch('https://gtfs.piemadd.com/data/cta/routes.json');
+    const stopsReq = await fetch('https://gtfs.piemadd.com/data/cta/stops.json');
+
+    const vehicleData = await vehicleReq.json();
+    const predictionsData = await predictionsReq.json();
+    const routesData = await routesReq.json();
+    const stopsData = await stopsReq.json();
+
+    const vehicleDataActual = vehicleData['bustime-response']['vehicle'][0];
+    const predictionsDataActual = predictionsData['bustime-response']['prd'];
+
+    let transitStatusObject = {
+      lastUpdated: now.toISOString(),
+      trains: {},
+      stations: {},
+      lines: {}
+    };
+
+    transitStatusObject[vehicleDataActual.rt] = {
+      lineCode: vehicleDataActual.rt,
+      lineNameShort: '',
+      lineNameLong: vehicleDataActual.rt,
+      routeColor: '882b2b',
+      routeTextColor: 'ffffff',
+      hasActiveTrains: true,
+      stations: routesData[vehicleDataActual.rt].routeStations,
+    }
+
+    routesData[vehicleDataActual.rt].routeStations.forEach((stationID) => {
+      transitStatusObject.stations[stationID] = {
+        stationID,
+        stationName: stopsData[stationID].stopName,
+        lat: stopsData[stationID].stopLat,
+        lon: stopsData[stationID].stopLon,
+        destinations: {}
+      }
+    })
+
+    predictionsDataActual.forEach((prediction) => {
+      transitStatusObject.stations[prediction.stpid].destinations[prediction.des] = {
+        trains: [
+          {
+            runNumber: prediction.vid,
+            actualETA: parseTime(prediction.prdtm),
+            noETA: false,
+            line: prediction.rt,
+            lineCode: prediction.rt,
+            lineColor: '882b2b',
+            lineTextColor: 'ffffff',
+          }
+        ]
+      }
+    })
+
+    transitStatusObject.trains[busNum] = {
+      lat: vehicleDataActual.lat,
+      lon: vehicleDataActual.lon,
+      heading: vehicleDataActual.hdg,
+      line: vehicleDataActual.rt,
+      lineCode: vehicleDataActual.rt,
+      lineColor: '882b2b',
+      lineTextColor: 'ffffff',
+      dest: vehicleDataActual.des,
+      predictions: predictionsDataActual.map((prediction) => {
+        return {
+          stationID: prediction.stpid,
+          stationName: prediction.stpnm,
+          actualETA: parseTime(prediction.prdtm),
+          noETA: false
+        }
+      }),
+    }
+
+    return transitStatusObject;
+  } catch (e) {
+    console.log("Error updating holiday bus:", e)
+    return false;
+  }
+
+};
+
+exports.update = update;
