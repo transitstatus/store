ab7a72afb173cb6fa0a472e7d3a3562d60be8430
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2024-01-04 01:12:29 -0600

    killed bsky bot, fixed CFA

--- endpoints/cfa/index.js
+++ endpoints/cfa/index.js
@@ -7,8 +7,8 @@ const updateFeed = (async (zoneTag) => {
   const now = new Date();
   const monthAgo = new Date(now.valueOf() - (1000 * 60 * 60 * 24 * 30));
 
-  const nowString = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
-  const monthAgoString = `${monthAgo.getFullYear()}-${monthAgo.getMonth() + 1}-${monthAgo.getDate()}`;
+  const nowString = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${(now.getDate()).toString().padStart(2, '0')}`;
+  const monthAgoString = `${monthAgo.getFullYear()}-${(monthAgo.getMonth() + 1).toString().padStart(2, '0')}-${(monthAgo.getDate()).toString().padStart(2, '0')}`;
 
   let query = {
     "query": `{\n  viewer {\n    zones(filter: {zoneTag: \"${zoneTag}\"}) {\n      totals: httpRequests1dGroups(limit: 10000, filter: {date_geq: \"${monthAgoString}\", date_lt: \"${nowString}\"}) {\n        uniq {\n          uniques\n          __typename\n        }\n        __typename\n      }\n      zones: httpRequests1dGroups(orderBy: [date_ASC], limit: 10000, filter: {date_geq: \"${monthAgoString}\", date_lt: \"${nowString}\"}) {\n        dimensions {\n          date\n          __typename\n        }\n        uniq {\n          uniques\n          __typename\n        }\n        sum {\n          browserMap {\n            pageViews\n            key: uaBrowserFamily\n            __typename\n          }\n          bytes\n          cachedBytes\n          cachedRequests\n          contentTypeMap {\n            bytes\n            requests\n            key: edgeResponseContentTypeName\n            __typename\n          }\n          clientSSLMap {\n            requests\n            key: clientSSLProtocol\n            __typename\n          }\n          countryMap {\n            bytes\n            requests\n            threats\n            key: clientCountryName\n            __typename\n          }\n          encryptedBytes\n          encryptedRequests\n          ipClassMap {\n            requests\n            key: ipType\n            __typename\n          }\n          pageViews\n          requests\n          responseStatusMap {\n            requests\n            key: edgeResponseStatus\n            __typename\n          }\n          threats\n          threatPathingMap {\n            requests\n            key: threatPathingName\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n`
@@ -26,6 +26,8 @@ const updateFeed = (async (zoneTag) => {
   });
   try {
     const data = await res.json();
+
+    console.log(data)
 
     if (data.data == null) throw new Error(data.error);
 
