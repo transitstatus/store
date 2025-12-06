f09e2d1e987cb837bc9fec13a1744a6ae7834917
piemadd <piero@piemadd.com>
2025-12-06 00:56:39 -0600

    fixed mff?

--- endpoints/mff/index.js
+++ endpoints/mff/index.js
@@ -62,27 +62,24 @@ const updateFeed = async () => {
       }
     };
 
-    const routeData = await Promise.all(
-      ['red_route_a_line', 'blue_route_b_line', 'green_route_c_line_']
-        .map((lineName) =>
-          fetch(`https://buswhere.com/furfest/routes/${lineName}?t=${now.valueOf()}&t=${now.valueOf() + 55421}&initial=true`, {
-            "headers": {
-              "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
-              "Accept": "application/json, text/javascript, */*; q=0.01",
-              "Accept-Language": "en-US,en;q=0.5",
-              "X-Requested-With": "XMLHttpRequest",
-              "Sec-Fetch-Dest": "empty",
-              "Sec-Fetch-Mode": "cors",
-              "Sec-Fetch-Site": "same-origin",
-              "Pragma": "no-cache",
-              "Cache-Control": "no-cache"
-            },
-          })
-            .then(response => response.json()))
-    );
+    const routeData = await fetch(`https://buswhere.com/furfest/routes/red_route_a_line?t=${now.valueOf()}&t=${now.valueOf() + 55421}&initial=true`, {
+      "headers": {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Language": "en-US,en;q=0.5",
+        "X-Requested-With": "XMLHttpRequest",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache"
+      },
+    })
+      .then(response => response.json());
 
     //pushing routes into transitstatus object
-    [redStatic, blueStatic, greenStatic].forEach((routeStatic, i) => {
+    const routeStaticData = [redStatic, blueStatic, greenStatic];
+    routeStaticData.forEach((routeStatic, i) => {
       const meta = customMeta[i];
       transitStatus.lines[meta.code] = {
         lineCode: meta.code,
@@ -119,64 +116,62 @@ const updateFeed = async () => {
       })
     })
 
-    routeData.forEach((line) => {
-      if (line.devices.length < 1) return;
-      const lineMeta = customMeta[customMetaIndexes[line.devices[0].name.split(' ')[0]]];
-      const updatedAt = new Date(line.address_updated_at).valueOf();
+    if (routeData.devices.length < 1) return;
+    const lineMeta = customMeta[customMetaIndexes[routeData.devices[0].name.split(' ')[0]]];
+    const updatedAt = new Date(routeData.address_updated_at).valueOf();
 
-      for (let i = 0; i < line.devices.length; i++) {
-        const device = line.devices[i];
+    for (let i = 0; i < routeData.devices.length; i++) {
+      const device = routeData.devices[i];
 
-        const busID = device.name.split(' ')[2];
-        transitStatus.trains[busID] = {
-          lat: device.position.lat,
-          lon: device.position.lon,
-          heading: 0,
+      const busID = device.name.split(' ')[2];
+      transitStatus.trains[busID] = {
+        lat: device.position.lat,
+        lon: device.position.lon,
+        heading: 0,
+        realTime: true,
+        deadMileage: false,
+        line: lineMeta.name,
+        lineCode: lineMeta.code,
+        lineColor: lineMeta.color,
+        lineTextColor: 'ffffff',
+        dest: '',
+        predictions: [],
+        extra: {
+          accessible: device.name.endsWith('♿️')
+        }
+      }
+
+      transitStatus.lines[lineMeta.code].hasActiveTrains = true;
+    }
+
+    Object.keys(routeData.other_routes_stop_eta).forEach((stopID) => {
+      routeData.other_routes_stop_eta[stopID].forEach((stopTime) => {
+        const busID = stopTime.device.split(' ')[2];
+
+        transitStatus.trains[busID].predictions.push({
+          stationID: stopDict[stopID],
+          stationName: stopNameDict[stopID],
+          actualETA: isNaN(stopTime.eta) ? updatedAt : updatedAt + (parseInt(stopTime.eta) * 1000),
+          noETA: false,
           realTime: true,
-          deadMileage: false,
+          extra: {
+            accessible: stopTime.device.endsWith('♿️')
+          }
+        });
+
+        const destination = stopDict[stopID] == 'convention-center' ? `${lineMeta.name} Loop` : 'Convention Center';
+        transitStatus.stations[stopDict[stopID]].destinations[destination].trains.push({
+          runNumber: busID,
+          actualETA: isNaN(stopTime.eta) ? updatedAt : updatedAt + (parseInt(stopTime.eta) * 1000),
+          noETA: false,
+          realTime: true,
           line: lineMeta.name,
           lineCode: lineMeta.code,
           lineColor: lineMeta.color,
-          lineTextColor: 'ffffff',
-          dest: '',
-          predictions: [],
-          extra: {
-            accessible: device.name.endsWith('♿️')
-          }
-        }
-
-        transitStatus.lines[lineMeta.code].hasActiveTrains = true;
-      }
-
-      Object.keys(line.other_routes_stop_eta).forEach((stopID) => {
-        line.other_routes_stop_eta[stopID].forEach((stopTime) => {
-          const busID = stopTime.device.split(' ')[2];
-
-          transitStatus.trains[busID].predictions.push({
-            stationID: stopDict[stopID],
-            stationName: stopNameDict[stopID],
-            actualETA: isNaN(stopTime.eta) ? updatedAt : updatedAt + (stopTime.eta * 1000),
-            noETA: false,
-            realTime: true,
-            extra: {
-              accessible: stopTime.device.endsWith('♿️')
-            }
-          })
-
-          const destination = stopDict[stopID] == 'convention-center' ? `${lineMeta.name} Loop` : 'Convention Center';
-          transitStatus.stations[stopDict[stopID]].destinations[destination].trains.push({
-            runNumber: busID,
-            actualETA: isNaN(stopTime.eta) ? updatedAt : updatedAt + (stopTime.eta * 1000),
-            noETA: false,
-            realTime: true,
-            line: lineMeta.name,
-            lineCode: lineMeta.code,
-            lineColor: lineMeta.color,
-            lineTextColor: 'ffffff'
-          })
-
-          transitStatus.trains[busID].dest = destination;
+          lineTextColor: 'ffffff'
         })
+
+        transitStatus.trains[busID].dest = destination;
       })
     })
     console.log(`Finished updating MFF Buses`)
