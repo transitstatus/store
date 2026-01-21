1307b3b00fce410971e52a601c3656e3c3c95781
piemadd <piero@piemadd.com>
2026-01-21 15:01:46 -0600

    added metrolink

--- endpoints/amtraker_platforms/index.js
+++ endpoints/amtraker_platforms/index.js
@@ -1,6 +1,11 @@
 const updateFeed = async () => {
   try {
-    let responseObject = {};
+    let responseObject = {
+      stations: {},
+      stations_w_src: {},
+    };
+
+    return responseObject;
 
     //fetching data
     const resultingData = await Promise.all([
@@ -10,7 +15,30 @@ const updateFeed = async () => {
       fetch(url).then(res => res.json())
     ));
 
-    console.log(resultingData)
+    const sourceNames = [
+      'New Jersey Transit',
+      'Moynihan Train Hall',
+    ];
+
+    resultingData.forEach((source, sourceIndex) => {
+      Object.keys(source.stations).forEach((stationCode) => {
+
+        if (!responseObject.stations[stationCode]) responseObject.stations[stationCode] = {};
+        if (!responseObject.stations_w_src[stationCode]) responseObject.stations_w_src[stationCode] = {};
+
+        Object.keys(source.stations[stationCode]).forEach((trainNumber) => {
+          const trainTrack = source.stations[stationCode][trainNumber];
+
+          if (trainTrack.length > 0) {
+            responseObject.stations[stationCode][trainNumber] = trainTrack;
+            responseObject.stations_w_src[stationCode][trainNumber] = {
+              platform: trainTrack,
+              source: sourceNames[sourceIndex],
+            }
+          }
+        });
+      });
+    });
 
     console.log(`Finished updating All Amtraker Tracks`)
     return responseObject;
---
+++ endpoints/metrolink_platforms/index.js
@@ -0,0 +1,77 @@
+const codeReplacements = {
+  'ARTIC': 'ANA',
+  'BURBANK-AIRPORT-SOUTH': 'BUR',
+  'CAMARILLO': 'CML',
+  'CHATSWORTH': 'CWT',
+  'DOWNTOWN BURBANK': 'BBK',
+  'FULLERTON': 'FUL',
+  'GLENDALE': 'GDL',
+  'IRVINE': 'IRV',
+  'LAUS': 'LAX',
+  'MOORPARK': 'MPK',
+  'NORTHRIDGE': 'NRG',
+  'OXNARD': 'OXN',
+  'RIVERSIDE-DOWNTOWN': 'RIV',
+  'SAN JUAN CAPISTRANO': 'SNC',
+  'SANTA ANA': 'SNA',
+  'SIMIVALLEY': 'SIM',
+  'VAN NUYS': 'VNC'
+};
+
+const updateFeed = async () => {
+  try {
+    let responseObject = {};
+    let unknownStations = [];
+
+    //fetching data
+    const res = await fetch("https://rtt.metrolinktrains.com/StationScheduleList.json");
+    const data = await res.json();
+
+    data
+      .filter((prediction) => prediction.TrainDesignation.startsWith('A'))
+      .forEach((prediction) => {
+        const trainNumber = prediction.TrainDesignation.replace('A', '');
+        const stationCode = codeReplacements[prediction.PlatformName];
+        const stationTrack = prediction.FormattedTrackDesignation.replace('Track ', '');
+
+        if (!stationCode) {
+          unknownStations.push(prediction.PlatformName);
+          return;
+        }
+
+        if (!responseObject[stationCode]) responseObject[stationCode] = {};
+        responseObject[stationCode][trainNumber] = stationTrack;
+      });
+
+
+    console.log(`Finished updating Metrolink tracks`)
+    return {
+      stations: responseObject
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      stations: {
+        "ANA": {},
+        "BUR": {},
+        "CML": {},
+        "CWT": {},
+        "BBK": {},
+        "FUL": {},
+        "GDL": {},
+        "IRV": {},
+        "LAX": {},
+        "MPK": {},
+        "NRG": {},
+        "OXN": {},
+        "RIV": {},
+        "SNC": {},
+        "SNA": {},
+        "SIM": {},
+        "VNC": {}
+      }
+    };
+  }
+};
+
+exports.update = updateFeed;
