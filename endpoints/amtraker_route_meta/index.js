165147e44711b6e177edbeadce232453a162ef50
Piero Maddaleni <piero@piemadd.com>
2026-07-20 03:30:27 -0500

    Fixed next dep info

--- endpoints/amtraker_route_meta/index.js
+++ endpoints/amtraker_route_meta/index.js
@@ -4,18 +4,25 @@ const providers = { amtrak: "Amtrak", brightline: "Brightline", via_rail: "Via" 
 
 const updateFeed = async (feed) => {
   //DEBUG
-  //if (feed != "amtrak") return false;
+  //if (feed != "brightline") return false;
 
   try {
-    const [staticStopsData, staticRoutesData, staticMetaData, atlasRoutesData, amtrakerStopsData] = await Promise.all(
-      [
-        `https://gtfs.piemadd.com/data/${feed}/stops.json`,
-        `https://gtfs.piemadd.com/data/${feed}/routes.json`,
-        `https://gobblerstatic.transitstat.us/schedules/${feed}/metadata.json`,
-        `http://localhost:3000/atlas_routes/${feed}/routeDataFinal`,
-        `https://api.amtraker.com/v3/stations`
-      ].map((url) => fetch(url).then((res) => res.json()))
-    );
+    const [staticStopsData, staticRoutesData, staticMetaData, atlasNextDeps, atlasRoutesData, amtrakerStopsData] =
+      await Promise.all(
+        [
+          `https://gtfs.piemadd.com/data/${feed}/stops.json`,
+          `https://gtfs.piemadd.com/data/${feed}/routes.json`,
+          `https://gobblerstatic.transitstat.us/schedules/${feed}/metadata.json`,
+          `https://gobblerstatic.transitstat.us/schedules/${feed}/nextDeps.json`,
+          `http://localhost:3000/atlas_routes/${feed}/routeDataFinal`,
+          `https://api.amtraker.com/v3/stations`
+        ].map((url) => fetch(url).then((res) => res.json()))
+      );
+
+    let shortTripToTrip = {};
+    Object.values(atlasNextDeps).forEach((trip) => {
+      shortTripToTrip[trip.trip.shortTripID] = trip.trip.tripID;
+    });
 
     const trainNumPrefix = feed.substring(0, 1).replace("a", "");
     const stopIDPrefix = feed == "brightline" ? "B" : "";
@@ -61,7 +68,8 @@ const updateFeed = async (feed) => {
               code: code,
               tz: thisStop?.tz ?? thisStopAlt?.stopTZ
             };
-          })
+          }),
+          nextDep: atlasNextDeps[shortTripToTrip[trainNum]]?.time ?? null
         };
       });
     });
